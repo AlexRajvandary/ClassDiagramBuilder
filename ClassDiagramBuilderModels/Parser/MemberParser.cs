fa8aa7ac48800370bc21f92bfdaf7611cdf12ede
Alex Rajvandary <alex.rajvandary@mostgames.org>
2022-10-22 22:15:57 +0300

    Start writing member parser.

--- ClassDiagramBuilderModels/Parser/MemberParser.cs
+++ ClassDiagramBuilderModels/Parser/MemberParser.cs
@@ -1,9 +1,15 @@
 ﻿using ClassDiagramBuilder.Models.TypeAnalyzerModels;
+using System.Text.RegularExpressions;
 
 namespace ClassDiagramBuilder.Models.Parser
 {
     public class MemberParser
     {
+        private readonly Regex classDeclarationPattern = new Regex(@"");
+        private readonly Regex fieldDeclarationPattern = new Regex(@"");
+        private readonly Regex propertyDeclarationPattern = new Regex(@"");
+        private readonly Regex methodDeclarationPattern = new Regex(@"");
+        private readonly Regex constructorDeclarationPattern = new Regex(@"");
         private readonly List<string> accsessModifiers = new List<string>() { "private", "public", "protected", "internal", "private protected", "protected internal" };
         private readonly List<string> typekind = new List<string>() { "class", "struct", "enum", "record", "interface", "delegate" };
         private const string abstractStr = "abstract";
@@ -13,33 +19,14 @@ namespace ClassDiagramBuilder.Models.Parser
         private const string constStr = "const";
         private const string usingStr = "using";
 
-        private MemberParser()
+        public MemberParser()
         {
 
         }
-        
-        public List<TypeInfo> GetFields()
-        {
-            var fields = new List<TypeInfo>();
-            return fields;
-        }
 
-        public List<TypeInfo> GetProperties()
+        public MemberInfo Parse(string data)
         {
-            var properties = new List<TypeInfo>();
-            return properties;
-        }
-
-        public List<MethodInfo> GetMethods()
-        {
-            var methods = new List<MethodInfo>();
-            return methods;
-        }
-
-        public List<ConstructorInfo> GetCtors()
-        {
-            var ctors = new List<ConstructorInfo>();
-            return ctors;
+            throw new NotImplementedException();
         }
     }
 }
