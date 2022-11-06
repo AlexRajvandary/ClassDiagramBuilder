447fba35373fbadea81567cae4abb8c840cc43dc
Alex Rajvandary <alex.rajvandary@mostgames.org>
2022-11-06 20:09:14 +0300

    PropertyInfo was added.

--- ClassDiagramBuilderModels/TypeAnalyzerModels/PropertyInfo.cs
+++ ClassDiagramBuilderModels/TypeAnalyzerModels/PropertyInfo.cs
@@ -1,6 +1,26 @@
 ﻿namespace ClassDiagramBuilder.Models.TypeAnalyzerModels
 {
-    public class PropertyInfo
+    public class PropertyInfo : MemberInfo
     {
+        public PropertyInfo(AcsessModifiers acsessModifier,
+                            AcsessModifiers getterAcsessModifier,
+                            AcsessModifiers setterAcsessModifier,
+                            bool isAbstract,
+                            bool isAuto,
+                            bool isStatic,
+                            string name,
+                            string nameSpace) : base(acsessModifier, isAbstract, isStatic, name, nameSpace)
+        {
+            GetterAcsessModifier = getterAcsessModifier;
+            IsAuto = isAuto;
+            SetterAcsessModifier = setterAcsessModifier;
+        }
+
+        public AcsessModifiers GetterAcsessModifier { get; private set; }
+
+        public bool IsAuto { get; private set; }
+
+        public AcsessModifiers SetterAcsessModifier { get; private set; }
+
     }
 }
