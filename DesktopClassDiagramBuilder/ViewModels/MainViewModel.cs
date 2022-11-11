46213f44faf8ed286444fb177f7a54746f5a7a29
Alex Rajvandary <alex.rajvandary@mostgames.org>
2022-11-11 18:07:56 +0545

    Slide bar added.

---
+++ DesktopClassDiagramBuilder/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassDiagramBuilder;
+using ClassDiagramBuilder.Models.TypeAnalyzerModels;
+
+namespace DesktopClassDiagramBuilder.ViewModels
+{
+    public class MainViewModel : PropertyChangedNotifier
+    {
+        public Graph<TypeInfo> Types { get; set; }
+    }
+}
