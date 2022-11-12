5943a6d1cf44f775508f9010b462a5cb385cdfc5
Alex Rajvandary <alex.rajvandary@mostgames.org>
2022-11-12 11:15:27 +0545

    UI improved.

--- DesktopClassDiagramBuilder/MainWindow.xaml.cs
+++ DesktopClassDiagramBuilder/MainWindow.xaml.cs
@@ -34,18 +34,12 @@ namespace DesktopClassDiagramBuilder
                 tt_home.Visibility = Visibility.Collapsed;
                 tt_contacts.Visibility = Visibility.Collapsed;
                 tt_messages.Visibility = Visibility.Collapsed;
-                tt_maps.Visibility = Visibility.Collapsed;
-                tt_settings.Visibility = Visibility.Collapsed;
-                tt_signout.Visibility = Visibility.Collapsed;
             }
             else
             {
                 tt_home.Visibility = Visibility.Visible;
                 tt_contacts.Visibility = Visibility.Visible;
                 tt_messages.Visibility = Visibility.Visible;
-                tt_maps.Visibility = Visibility.Visible;
-                tt_settings.Visibility = Visibility.Visible;
-                tt_signout.Visibility = Visibility.Visible;
             }
         }
 
