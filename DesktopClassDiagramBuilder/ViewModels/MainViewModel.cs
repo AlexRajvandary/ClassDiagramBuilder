fa5ac874ce9020f6dd88a31ab076b3b2cc1d0efa
Alex Rajvandary <alex.rajvandary@mostgames.org>
2022-11-14 16:50:01 +0545

    Added textbox and button to test parser.

--- DesktopClassDiagramBuilder/ViewModels/MainViewModel.cs
+++ DesktopClassDiagramBuilder/ViewModels/MainViewModel.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using ClassDiagramBuilder;
 using ClassDiagramBuilder.Models;
 using ClassDiagramBuilder.Models.TypeAnalyzerModels;
@@ -12,18 +13,49 @@ namespace DesktopClassDiagramBuilder.ViewModels
 {
     public class MainViewModel : PropertyChangedNotifier
     {
+        ICommand calculate;
+        private string path;
+        private Graph<TypeInfo> types;
         private TypeInfo typeInfo;
         private static ProjectAnalyzer projectAnalyzer;
 
         public MainViewModel()
         {
             projectAnalyzer = new ProjectAnalyzer();
-            TypeInfo = projectAnalyzer.AnalyzeFile(@"C:\Users\Alex Raj\source\repos\ClassDiagramBuilder\ClassDiagramBuilder\Program.cs").FirstOrDefault();
+            calculate = new RelayCommand(GetTypeInfos);
         }
 
-        public Graph<TypeInfo> Types { get; set; }
+        public ICommand Calculate
+        {
+            get => calculate;
+            set
+            {
+                calculate = value;
+                OnPropertyChanged();
+            }
+        }
 
-        public TypeInfo TypeInfo 
+        public string Path
+        {
+            get => path;
+            set
+            {
+                path = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Graph<TypeInfo> Types
+        {
+            get => types;
+            set
+            {
+                types = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public TypeInfo TypeInfo
         {
             get => typeInfo;
             set
@@ -31,6 +63,11 @@ namespace DesktopClassDiagramBuilder.ViewModels
                 typeInfo = value;
                 OnPropertyChanged();
             }
+        }
+
+        public void GetTypeInfos()
+        {
+            TypeInfo = projectAnalyzer?.AnalyzeFile(Path)?.FirstOrDefault();
         }
     }
 }
---
+++ DesktopClassDiagramBuilder/ViewModels/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DesktopClassDiagramBuilder.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+        private readonly Action action;
+
+        public RelayCommand(Action action)
+        {
+            this.action = action;
+        }
+
+        public bool CanExecute(object? parameter) => true;
+
+        public void Execute(object? parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                action?.Invoke();
+            }
+        }
+    }
+}
